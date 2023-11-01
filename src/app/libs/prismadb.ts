ebcf7bb29024d747a8871dc8b3cf7d46333501ff
vinhdnvn <vinh120203@gmail.com>
2023-11-01 20:59:29 +0700

    api: register

--- src/app/libs/prismadb.ts
+++ src/app/libs/prismadb.ts
@@ -5,6 +5,6 @@ declare global {
 }
 
 const client = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== 'production') global.prisma = client;
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
 
 export default client;
